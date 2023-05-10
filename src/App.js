b5f0eccf4ed9e2672d612bf542641cbb901555b0
milli <milli2021k@gmail.com>
2023-05-10 18:42:52 -0500

    created  separate components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from "./logo.svg";
+
 import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
+import React from 'react'
+
+
 
 function App() {
   return (
-    <div className="App">
-
-      <img src={logo} />
-      <h1>Fun Facts About React</h1>
-
-      <ol>
-        <li>was First releaased in 2013</li>
-        <li>was orignally created by Jordan</li>
-        <li>has 100k plus stars on Github</li>
-
-        <li>is maintained by facebook</li>
-
-        <li>powers thousands of enterprise apps and mobile apps</li>
-      </ol>
-    </div>
+   <>
+   <Header/>
+   <MainContent/>
+   <Footer/>
+   </>
   );
 }
 
