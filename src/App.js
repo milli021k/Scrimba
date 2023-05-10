b6626a9fa16e1c2f903f59064a9870628b6c4e9a
milli <milli2021k@gmail.com>
2023-05-10 17:58:38 -0500

    coming vs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <img src={logo} />
- <h1>Fun Facts About React</h1>
+      <h1>Fun Facts About React</h1>
 
- <ol>
-  <li>
-was First releaased in 2013
-  </li>
-  <li>
-was orignally created by Jordan 
-  </li>
-  <li>
+      <ol>
+        <li>was First releaased in 2013</li>
+        <li>was orignally created by Jordan</li>
+        <li>has 100k plus stars on Github</li>
 
-  has 100k plus stars on Github
-</li>
-  
-  <li>
-    is maintained by facebook
-  </li>
+        <li>is maintained by facebook</li>
 
-  <li>
-    powers thousands of enterprise apps and mobile apps
-  </li>
-
- </ol>
+        <li>powers thousands of enterprise apps and mobile apps</li>
+      </ol>
     </div>
   );
 }
